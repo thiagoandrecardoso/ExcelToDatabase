e335c712e1c41f1df4e0fe7dd2485f0869039e0c
Thiago <thiagoandrecardoso@icloud.com>
2022-11-19 14:44:47 -0300

    Added shared class - methods excel read

---
+++ src/main/java/who/programador/excel/shared/ExcelMethodShared.java
@@ -0,0 +1,26 @@
+package who.programador.excel.shared;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class ExcelMethodShared {
+    public static Workbook getWorkbook(String excelFilePath) {
+        try {
+            FileInputStream inputStream = new FileInputStream(excelFilePath);
+            return new XSSFWorkbook(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Iterator<Row> getRowIterator(Workbook workbook) {
+        Sheet firstSheet = workbook.getSheetAt(0);
+        return firstSheet.iterator();
+    }
+}
